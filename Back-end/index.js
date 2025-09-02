2a5b692dda3df0a0e26310d31f3b3b1046369c75
zahoor khan <zahoorkhan112257@gmail.com>
2025-09-02 11:56:23 +0500

    adjusting cors configuration

--- Back-end/index.js
+++ Back-end/index.js
@@ -10,7 +10,12 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors());
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3000'].filter(Boolean),
+  allowedHeaders: ['Content-Type','Authorization'],
+  methods: ['GET','POST','PUT','DELETE','OPTIONS']
+}));
+
 
 
 
