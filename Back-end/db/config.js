e4f4009efb1aa162cc6d28f491f9ef7fdebad009
zahoor khan <zahoorkhan112257@gmail.com>
2025-09-03 19:21:13 +0500

    preparing back-end for cloud and local database connection

--- Back-end/db/config.js
+++ Back-end/db/config.js
@@ -3,15 +3,15 @@ require('dotenv').config(); // load .env variables
 
 const dbconnection = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/E-dashboard", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => {
-      console.log('✅ Database connected successfully.');
+      console.log(' Database connected successfully.');
     })
     .catch((err) => {
-      console.error('❌ Error while connecting to DB:', err.message);
+      console.error(' Error while connecting to DB:', err.message);
     });
 };
 
